0903fcf6fdb20893d94682b961f64b38f0654579
Danimartinez-1997 <martinezviladaniel@gmail.com>
2025-07-02 08:10:36 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key                  = "mock"
+  secret_key                  = "mock"
+  region                      = "us-east-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+module "bucket_localstack" {
+  source      = "./modules/mi-recurso"
+  bucket_name = "bucket-desde-modulo"
+}
